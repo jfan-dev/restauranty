7937d3828e3b50d81943ffa1fb2e1613be3c8aed
jfan-dev <jfan.dev+github@gmail.com>
2024-06-14 22:57:08 -0300

    :railway_track: Add SignUp route to the router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import SignIn from '../components/SignIn.vue';
-import WelcomeView from '../views/WelcomeView.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import WelcomeView from '../views/WelcomeView.vue'
+import SignIn from '../components/SignIn.vue'
+import SignUp from '../components/SignUp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  {
-  path: '/',
-  name: 'welcome',
-  component: WelcomeView
-  },
-  {
-  path: '/signin',
-  name: 'signin',
-  component: SignIn
-  }
+    {
+      path: '/',
+      name: 'welcome',
+      component: WelcomeView
+    },
+
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn
+    },
+
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    }
   ]
- })
+})
 
 export default router;
