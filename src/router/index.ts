eb533a66fffaa5f9b76bc0695f72e1f8b368af1b
jfan-dev <jfan.dev+github@gmail.com>
2024-06-17 18:47:12 -0300

    🚦 Update Vue Router to include route for AddProduct component

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,15 @@ import SignIn from '@/components/SignIn.vue'
 import SignUp from '@/components/SignUp.vue'
 import StoresList from '@/components/StoresList.vue'
 import StoreProducts from '@/components/StoreProducts.vue'
+import AddProduct from '@/components/AddProduct.vue'
 
 const routes = [
   { path: '/', name: 'welcome', component: WelcomeView },
   { path: '/signin', name: 'signin', component: SignIn },
   { path: '/signup', name: 'signup', component: SignUp },
   { path: '/stores', name: 'stores', component: StoresList },
-  { path: '/stores/:id/products', name: 'store-products', component: StoreProducts, props: true }
+  { path: '/stores/:id/products', name: 'store-products', component: StoreProducts, props: true },
+  { path: '/stores/:id/products/new', name: 'add-product', component: AddProduct, props: true }
 ]
 
 const router = createRouter({
