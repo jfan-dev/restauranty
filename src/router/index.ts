e48c821af60dc68209086fab70e52a0d26aeae59
jfan-dev <jfan.dev+github@gmail.com>
2024-06-17 15:04:42 -0300

    ✨ feat(router): add routes for StoresList and StoreProducts components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomeView from '../views/WelcomeView.vue'
-import SignIn from '../components/SignIn.vue'
-import SignUp from '../components/SignUp.vue'
+import WelcomeView from '@/views/WelcomeView.vue'
+import SignIn from '@/components/SignIn.vue'
+import SignUp from '@/components/SignUp.vue'
+import StoresList from '@/components/StoresList.vue'
+import StoreProducts from '@/components/StoreProducts.vue'
+
+const routes = [
+  { path: '/', name: 'welcome', component: WelcomeView },
+  { path: '/signin', name: 'signin', component: SignIn },
+  { path: '/signup', name: 'signup', component: SignUp },
+  { path: '/stores', name: 'stores', component: StoresList },
+  { path: '/stores/:id/products', name: 'store-products', component: StoreProducts, props: true }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'welcome',
-      component: WelcomeView
-    },
-
-    {
-      path: '/signin',
-      name: 'signin',
-      component: SignIn
-    },
-
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignUp
-    }
-  ]
+  routes
 })
 
-export default router;
+export default router
